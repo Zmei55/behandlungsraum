25d3f98b6bf246394a75dc801735024209da83a8
Andrei <97633022+Zmei55@users.noreply.github.com>
2022-10-06 10:51:21 +0200

    theme added

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,12 +1,37 @@
 export const theme = Object.freeze({
   colors: {
-    first: '#0d3dff',
-    secondary: '#9caffa',
-    white: '#fff',
-    black: 'rgb(59, 59, 59)',
-    grey: '#979797',
+    text: {
+      default: '#1C1B3B',
+      body: '#524E6B',
+      label: '#6B6F91',
+      placeholder: '#9DA0BD',
+    },
+    primary: {
+      pressed: '#0E4CC9',
+      hover: '#0F55E3',
+      default: '#115FFD',
+      disabled: 'rgba(17, 95, 253, 0.5)',
+    },
+    secondary: {
+      pressed: '#AEB1C2',
+      hover: '#C5C9DB',
+      default: '#DCE0F5',
+      disabled: 'rgba(220, 224, 245, 0.5)',
+    },
+    succes: {
+      pressed: '#126641',
+      hover: '#168051',
+      default: '#1B9962',
+      disabled: 'rgba(27, 153, 98, 0.5)',
+    },
+    error: {
+      pressed: '#BA2625',
+      hover: '#D42C2A',
+      default: '#EC312F',
+      disabled: 'rgba(236, 49, 47, 0.5)',
+    },
   },
-  borderForm: '1px solid #111',
-  borderRadius: '4px',
+  border: '2px solid #E6E6E6',
+  borderRadius: '16px',
   spacing: value => `${4 * value}px`,
 });
