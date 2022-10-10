a63cfa2e98d15fddf7498c0e8501233a50c1c842
Andrei <97633022+Zmei55@users.noreply.github.com>
2022-10-10 12:53:23 +0200

    add modal Patient

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,35 @@
+import { useCallback } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Backdrop, Content } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export function Modal({ children }) {
+export function Modal({ children, onClose }) {
+  const handleKeyDown = useCallback(
+    event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+  const handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      onClose();
+    }
+  };
+
   return createPortal(
-    <Backdrop>
+    <Backdrop onClick={handleBackdropClick}>
       <Content>{children}</Content>
     </Backdrop>,
     modalRoot
--- src/components/Patient/Patient.jsx
+++ src/components/Patient/Patient.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+import { Modal } from 'components/Modal';
 import { Container, ModalBtn, IconSelectPatient } from './Patient.styled';
 
 export function Patient() {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <Container>
-      <ModalBtn type="button">
+      <ModalBtn type="button" onClick={toggleModal} aria-label="find patient">
         <IconSelectPatient />
         Выбрать пациента
       </ModalBtn>
+
+      {showModal && <Modal onClose={toggleModal}></Modal>}
     </Container>
   );
 }
